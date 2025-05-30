2d452ac928d4d51890df20e3e20cd99e59b85b1a
momo <1668817352@qq.com>
2025-05-30 15:33:22 +0800

    整理文档

--- conf.py
+++ conf.py
@@ -17,22 +17,16 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'keyes WiKi'
-copyright = 'keyes , WiKi'
-author = 'keyes WiKi'
-copyright = 'Shenzhen keyes Technology Co., Ltd'
-
-root_doc = "index"
-master_doc = "index"
+project = 'Smart home Kit'
+copyright = 'keyestudio'
+author = 'keyestudio'
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_copybutton']
-
-
+extensions = ["sphinx_copybutton"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,12 +42,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 html_theme_options = {
   "show_toc_level": 3,
+  "show_navbar_depth": 2,
+  "max_navbar_depth": 2,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
